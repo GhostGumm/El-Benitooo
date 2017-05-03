47214e2b3fc3fcff1e2ddbcc7b8d1874b036d21e
Hakim <elbakk_a@outlook.com>
2017-05-03 23:10:12 +0200

    test push

---
+++ app/src/main/java/com/example/hakim/fbconnect/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.hakim.fbconnect;
+
+import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        loginButton = (LoginButton)findViewById(R.id.fb_login_bn);
+        textView = (TextView)findViewById(R.id.textView);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                textView.setText("Login Success \n" +
+                loginResult.getAccessToken().getUserId() + "\n" + loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+                textView.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    // Example of a call to a native method
+    TextView tv = (TextView) findViewById(R.id.sample_text);
+    tv.setText(stringFromJNI());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
